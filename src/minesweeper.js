b78992b9efc8c1f9cd011f3fb6291e284926dfc7
Daniel Pettifer <danielpettifer@me.com>
2018-10-30 07:24:22 +0000

    Board layout example

---
+++ src/minesweeper.js
@@ -0,0 +1,11 @@
+var blankLine = ' | | ';
+console.log('This is what an empty board will look like');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+var guessLine = '1| | ';
+var bombLine = ' |B| ';
+console.log('This is what a board ,ight look like during a game');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
